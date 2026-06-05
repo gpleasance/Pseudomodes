818376dfc24d43820be55734ff6176b982bbd0e7
Graeme Pleasance <GraemePleasance@MacBook-Pro.local>
2026-06-05 20:31:02 +0200

    Initial commit

---
+++ pseudomodes/siam.py
@@ -0,0 +1,7 @@
+"""
+Module for applying the pseudomode method and exact approaches to simulate the single-impurity Anderson model (SIAM).
+"""
+import numpy as np
+import qutip as qt
+from numpy import ndarray
+from qutip import Qobj
